81e383deb66c567d318754fe45ec745a7e732f59
Daro Saleh <darosaleh@gmail.com>
2025-01-24 23:38:48 +0000

    added pytest and initial test setup

---
+++ backend/logbook/tests/test_user_serializer.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from logbook.serializers import UserSerializer
+
+class UserSerializerTest(TestCase):
+
+    def test_valid_user_serializer(self):
+        valid_data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password': 'testpass123',
+            'confirm_password': 'testpass123'
+        }
+        serializer = UserSerializer(data=valid_data)
+        self.assertTrue(serializer.is_valid())
+
+    
+    def test_passwords_not_matching(self):
+        valid_data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password': 'testpass123',
+            'confirm_password': 'testpass1234'
+        }
+        serializer = UserSerializer(data=valid_data)
+        self.assertFalse(serializer.is_valid())
+
+    
